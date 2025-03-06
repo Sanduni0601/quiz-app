5057facc4151a2acbe265dede0c27516714ff12f
Sanduni0601 <sanduni06nisansala@gmail.com>
2025-03-06 10:39:23 +0530

    Create Start page

---
+++ my-app/src/components/Start.jsx
@@ -0,0 +1,10 @@
+export default function Start(){
+    return(
+        <div className="start">
+      <h1>Let's Go</h1>
+      <button className="startButton" onClick={handleClick}>
+        Start
+      </button>
+    </div>
+    )
+}
