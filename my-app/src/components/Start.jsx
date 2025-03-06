b546c079789af75bdbf8676e85235e9977ccf86d
Sanduni0601 <sanduni06nisansala@gmail.com>
2025-03-06 10:57:59 +0530

    Update

--- my-app/src/components/Start.jsx
+++ my-app/src/components/Start.jsx
@@ -1,10 +1,10 @@
-export default function Start(){
-    return(
+export default function Start({ setStartGame }) {
+    return (
         <div className="start">
-      <h1>Let's Go</h1>
-      <button className="startButton" onClick={handleClick}>
-        Start
-      </button>
-    </div>
-    )
+            <h1>Let's Go</h1>
+            <button className="startButton" onClick={() => setStartGame(true)}>
+                Start
+            </button>
+        </div>
+    );
 }
